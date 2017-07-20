dc917cb751ac6d89ff6b474cc1660e0471a004e7
smoczis <romanowski.mateusz93@gmail.com>
2017-07-20 15:14:16 +0200

    Load and save properly in main file

--- application.py
+++ application.py
@@ -1,12 +1,19 @@
 from controller import *
 from events import *
 from view import *
+from DAO import *
 
 
 def main():
+
+    chckp_dao = CheckpointDAO()
+    mntr_dao = MentoringDAO()
+    chckp_dao.load_checkpoints()
+    mntr_dao.load_mentorings()
     controller = Controller()
     controller.start()
-    print('siemaeniu')
+    chckp_dao.save_checkpoints()
+    mntr_dao.save_mentorings()
 
 
 if __name__ == "__main__":
